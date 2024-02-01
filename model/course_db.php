fa19416fb8554f047bc9e47f9df6c5d2062073b3
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-01 13:52:04 +0200

    add course function

--- model/course_db.php
+++ model/course_db.php
@@ -43,3 +43,14 @@ function delete_course($course_id)
     $statement->execute();
     $statement->closeCursor();
 }
+
+function add_course($course_name)
+{
+    global $db;
+    $query = 'INSERT INTO courses (courseName) VALUES (:courseName)';
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':courseName', $course_name);
+    $statement->execute();
+    $statement->closeCursor();
+}
